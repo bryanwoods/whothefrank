5288ca43735c98bf6adf91e8f3f9260ebaedd01e
Bryan Woods <bryanwoods4e@gmail.com>
2009-11-23 16:02:00 -0500

    Fixing link

--- application.rb
+++ application.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "Try this: <a href='http://www.whothefrank.com/whois/cheapramen.com'> + 
-    http://www.whothefrank.com/whois/cheapramen.com</a>"
+  "Try this: <a href='http://www.whothefrank.com/whois/cheapramen.com'>" + 
+    "http://www.whothefrank.com/whois/cheapramen.com</a>"
 end
 
 get '/whois/:domain' do |domain|
