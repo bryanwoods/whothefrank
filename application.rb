72565b28d4306fbe28f6387cdd9ca4c6662d68af
Bryan Woods <bryanwoods4e@gmail.com>
2009-11-23 16:03:25 -0500

    Change 'whois' to 'lookup' in URL

--- application.rb
+++ application.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  "Try this: <a href='http://www.whothefrank.com/whois/cheapramen.com'>" + 
-    "http://www.whothefrank.com/whois/cheapramen.com</a>"
+  "Try this: <a href='http://www.whothefrank.com/lookup/cheapramen.com'>" + 
+    "http://www.whothefrank.com/lookup/cheapramen.com</a>"
 end
 
-get '/whois/:domain' do |domain|
+get '/lookup/:domain' do |domain|
   %x{ whois #{domain} }.gsub("\n", "<br />")
 end
