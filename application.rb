3f7b1ec341c213561d5894bdbf7cafa006fa52c5
Bryan Woods <bryanwoods4e@gmail.com>
2009-12-11 01:56:09 -0500

    Let varnish get its freak on

--- application.rb
+++ application.rb
@@ -4,10 +4,12 @@ require 'environment'
 include ApplicationHelpers
 
 get '/' do
+  response.headers['Cache-Control'] = 'public, max-age=1000'
   haml(:index, :format => :html5)
 end
 
 get '/lookup/:domain' do |domain|
+  response.headers['Cache-Control'] = 'public, max-age=1000'
   @domain = domain.capitalize
   @whois_result = %x{ whois #{domain} }.gsub("\n", "<br />\n")
   haml(:result, :format => :html5)
