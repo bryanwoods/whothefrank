825bfdc00aeb5d9afed30d897535c524cc4030f0
Bryan Woods <bryanwoods4e@gmail.com>
2010-02-26 11:30:55 -0500

    Set views to render in ugly mode. Clean up some day.

--- application.rb
+++ application.rb
@@ -5,40 +5,40 @@ include ApplicationHelpers
 
 get '/' do
   response.headers['Cache-Control'] = 'public, max-age=1000'
-  haml(:index, :format => :html5)
+  haml(:index, :format => :html5, :ugly => true)
 end
 
 get '/whois/:domain' do |domain|
   response.headers['Cache-Control'] = 'public, max-age=1000'
   @domain = domain.capitalize
   @result = %x{ whois #{domain} }.gsub("\n", "<br />\n")
-  haml(:result, :format => :html5)
+  haml(:result, :format => :html5, :ugly => true)
 end
 
 get '/dig/:domain' do |domain|
   response.headers['Cache-Control'] = 'public, max-age=1000'
   @domain = domain.capitalize
   @result = %x{ dig #{domain} }.gsub("\n", "<br />\n")
-  haml(:result, :format => :html5)
+  haml(:result, :format => :html5, :ugly => true)
 end
 
 get '/head/:domain' do |domain|
   response.headers['Cache-Control'] = 'public, max-age=1000'
   @domain = domain.capitalize
   @result = %x{ curl -I #{domain} }.gsub("\n", "<br />\n")
-  haml(:result, :format => :html5)
+  haml(:result, :format => :html5, :ugly => true)
 end
 
 get '/nslookup/:domain' do |domain|
   response.headers['Cache-Control'] = 'public, max-age=1000'
   @domain = domain.capitalize
   @result = %x{ nslookup #{domain} }.gsub("\n", "<br />\n")
-  haml(:result, :format => :html5)
+  haml(:result, :format => :html5, :ugly => true)
 end
 
 get '/curl/:domain' do |domain|
   response.headers['Cache-Control'] = 'public, max-age=1000'
   @domain = domain.capitalize
   @result = %x{ curl #{domain} }.gsub("\n", "<br />\n")
-  haml(:curl, :format => :html5)
+  haml(:curl, :format => :html5, :ugly => true)
 end
